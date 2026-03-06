d5cd7e85e4ccc285373a9662aedd1a357db70b0c
rohithkarthikeyan <rohithkarthikeyan@yahoo.com>
2026-03-06 14:25:51 -0600

    Initial Commit

---
+++ Bevoholic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bevoholic
+//
+//  Created by Rohith Karthikeyan on 3/6/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BevoholicTests/BevoholicTests.swift
@@ -0,0 +1,17 @@
+//
+//  BevoholicTests.swift
+//  BevoholicTests
+//
+//  Created by Rohith Karthikeyan on 3/6/26.
+//
+
+import Testing
+@testable import Bevoholic
+
+struct BevoholicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
